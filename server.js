aa97ee5a057be0a19ebd0be59cdf52241f8240f8
morteza.ataiy@gmail.com <morteza.ataiy@gmail.com>
2020-07-11 15:35:33 +0430

    fix Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const orm = require("orm");
-
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const dbConnectionInfo = require("./config/keys").dbConnectionInfo;
